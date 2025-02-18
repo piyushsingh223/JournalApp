5f679147f6beae035cecf3072861109e343c1555
Piyush <singhpiyush8128@gmail.com>
2025-02-18 15:44:18 +0530

    fixing build and analyze

--- src/main/java/com/maqsoft/journalApp/repository/JournalEntryRepo.java
+++ src/main/java/com/maqsoft/journalApp/repository/JournalEntryRepo.java
@@ -3,7 +3,9 @@ package com.maqsoft.journalApp.repository;
 import com.maqsoft.journalApp.Entity.JournalEntry;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JournalEntryRepo extends MongoRepository<JournalEntry, ObjectId> {
 
 }
--- src/main/java/com/maqsoft/journalApp/repository/UserRepo.java
+++ src/main/java/com/maqsoft/journalApp/repository/UserRepo.java
@@ -4,7 +4,9 @@ import com.maqsoft.journalApp.Entity.JournalEntry;
 import com.maqsoft.journalApp.Entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends MongoRepository<User, ObjectId> {
     User findByuserName(String username);
 
