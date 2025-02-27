fca1aad8f433b309237b771a0d8d3af0d48fa9a0
Piyush <singhpiyush8128@gmail.com>
2025-02-27 16:44:26 +0530

    added email sender and its test

---
+++ src/test/java/com/MAQ/JournalApp/Service/EmailServiceTest.java
@@ -0,0 +1,18 @@
+package com.MAQ.JournalApp.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTest {
+
+    @Autowired
+    private  EmailService emailservice;
+
+    @Test
+    void testSendMail(){
+        emailservice.sendEmail("iamfromkota27@gmail.com","java mail sending test","Hii , " +
+                "this is a dummy mail just to test the java mail sender i build");
+    }
+}
